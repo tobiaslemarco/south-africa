c3f22bd67f44aad3b983037a32edd73db735dfc2
Lemarco Tobias <tobiaslemarco@gmail.com>
2021-05-03 11:19:05 +0300

    second

--- main.js
+++ main.js
@@ -1,5 +1,1 @@
-Hi.
-My gmail is tobiaslemarco@gmail.com
-And then, don't mention it on site. They can block my account.
-I will do my best for you and your project with my good skills.
-Thanks!
+const a=5;
